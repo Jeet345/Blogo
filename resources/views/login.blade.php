a5750595ebd55a9cdfa102cc36798452b639e429
jeet <mendaparajeet@gmail.com>
2021-07-28 23:12:01 +0530

    Third

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -352,7 +352,7 @@
 
             $.ajax({
                 url: "{{ url('/login/forgotFormRequest') }}",
-                type: "POST",
+                type: "post",
                 data: formData,
                 success: function(data) {
 
